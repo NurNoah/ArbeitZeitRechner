aa9570325464d077ee6c3653259a102877fcf2a2
Denis Shekerov <denis.shekerov@dachser.com>
2023-12-22 08:02:02 +0100

    add Android version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,19 +9,30 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TuiInputTimeModule } from '@taiga-ui/kit';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TuiInputTimeModule,
+    NgxMaterialTimepickerModule
   ],
-  providers: [],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { isEmpty } from 'rxjs';
 import { DarkmodeserviceService } from '../darkmodeservice.service';
 import { CalcService } from '../calc.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { tuiCreateTimePeriods } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +13,11 @@ import { CalcService } from '../calc.service';
 
 export class HomeComponent {
 
-  constructor(public ds: DarkmodeserviceService, public c: CalcService) {}
+  constructor(public ds: DarkmodeserviceService, public c: CalcService) { }
+
+  
+
+  items1 = tuiCreateTimePeriods();
+  items2 = tuiCreateTimePeriods(10, 20, [0, 15, 30, 45]);
 
 }
-// :)
